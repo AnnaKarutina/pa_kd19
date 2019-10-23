53135619b9208336272176f125577e5125c57859
Anna Karutina <anna.karutina@khk.ee>
2019-10-23 16:24:51 +0300

    Konsooli kasutamisvõimalused info väljastamiseks

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// Andmetüübid
+// Logid konsoolile
 
-// täisarvud
-console.log(10);
-console.log(-4);
-console.log(0);
+// logid
+console.log('123');
 
-// reaalarvud - ujuvkomaarvud
-console.log(2.5);
-console.log(-7.23);
-console.log(-2.0);
-
-// sõned - tekst
-console.log('Hello World!');
-
-// tõeväärtused
-console.log(true); // tõene
-console.log(false); // väär
-
-// nimekiri 
-console.log([1, 2, 3, 4]);
-
-// objektid
-console.log({a : 1, b : 2});
+// tabel
 console.table({a : 1, b : 2});
+
+// veateade
+console.error('See on mingi viga');
+
+// märkused
+console.warn('See on märkus');
+
+// ajakulu kontroll
+console.time('Test');
+  console.log('mingi tegevus');
+  console.log('mingi tegevus');
+  console.log('mingi tegevus');
+  console.log('mingi tegevus');
+  console.log('mingi tegevus');
+console.timeEnd('Test');
+
+// kustutamine
+console.clear();
