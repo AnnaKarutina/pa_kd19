aa6e03d5a7bf51a8e9aa6836b9bc8a64648e28e8
Anna Karutina <anna.karutina@khk.ee>
2019-10-23 16:50:15 +0300

    Muutujate initsialiseerimine, defineerimine ja väärtuste omistamine

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-// Logid konsoolile
+// Muutujad
 
-// logid
-console.log('123');
+// var
+// muutujate defineerimine
+var nimi = 'Anna'; // defineerimine
+console.log(nimi);
+nimi = 'Katrin'; // väärtuse omistamine
+console.log(nimi);
 
-// tabel
-console.table({a : 1, b : 2});
+// muutujate initsialiseerimine
+var tervitus;
+console.log(tervitus);
+tervitus = 'Tere tulemast!';
+console.log(tervitus);
 
-// veateade
-console.error('See on mingi viga');
+// liitnimede kasutamine
+var eesNimi = 'Anna'; // Camel Stiil
+var ees_nimi = 'Anna'; // PHP Stiil
+var EesNimi = 'Anna'; // Pascal Stiil
+var eesnimi = 'Anna';
 
-// märkused
-console.warn('See on märkus');
+// let - muutuja
+let pereNimi = 'Karutina';
+console.log(pereNimi);
+pereNimi = '?????';
+console.log(pereNimi);
 
-// ajakulu kontroll
-console.time('Test');
-  console.log('mingi tegevus');
-  console.log('mingi tegevus');
-  console.log('mingi tegevus');
-  console.log('mingi tegevus');
-  console.log('mingi tegevus');
-console.timeEnd('Test');
+// const - konstant
+const minuEesNimi = 'Anna';
+console.log(minuEesNimi);
+// minuEesNimi = 'Katrin';
 
-// kustutamine
-console.clear();
+// const kasutamise näide 1
+const inimene = {nimi : 'Katrin', vanus : 20};
+// inimene = {nimi: 'Katrin', rahvus: 'eestlane'};
+console.table(inimene);
+inimene.vanus = 21;
+console.table(inimene);
+
+// const kasutamine näide 2
+const arvud = [1, 2, 3, 4, 5];
+// arvud = [1, 2, 3, 4, 5, 6];
+console.log(arvud);
+arvud.push(6);
+console.log(arvud);
