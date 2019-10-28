56db94a052f18a8aed288cd746da72b4caa86c26
Anna Karutina <anna.karutina@khk.ee>
2019-10-28 11:46:59 +0200

    while - algarvu leidmine

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 
 // while
 // ellkontrolliga tsükkel
-let number = 1; // defineerime tsükli juhtimismuutuja
-// paneme kontrolltingimus paika
-while(number <= 10) {
-  // sooritame tegevused
-  console.log(number);
-  // suurendame juhtimismuutuja väärtus
-  number++;
+// defineerime number
+let arv = 4; 
+// defineerime jagaja algväärtus
+let jagaja = 2;
+// suurendame jagaja väärtus nii kaua, kui
+// jagamise jääk on 0
+while(arv % jagaja != 0){
+  jagaja++;
+}
+// kontrollime, kas jagasime sama arvuga
+if(arv == jagaja) {
+  console.log(arv + ' on algarv');
+} else {
+  console.log(arv + ' ei ole algarv');
 }
