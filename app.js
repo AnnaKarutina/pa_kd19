224a24c0a248318b0194e3e9fefeb2b47698bd03
Anna Karutina <anna.karutina@khk.ee>
2019-10-28 11:03:15 +0200

    continue operaatori kasutamise näide - ainult paaris arvud

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 // for tsükkel
 // üldtsükkel
-// let arv;
 let lause;
 for(let arv = 1; arv <= 10; arv++){
   lause = 'arv =  ';
@@ -14,6 +13,7 @@ for(let arv = 1; arv <= 10; arv++){
     lause = lause + ' - paaris';
   } else {
     lause = lause + ' - paaritu';
+    continue;
   }
   console.log(lause);
 }
